8904772e4bca95fd91cde13be9f4b9f64fb12119
jarangit <jaran.dch@gmail.com>
2024-06-09 03:13:32 +0700

    feat: create why section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Banner from "./components/sections/banner";
 import GetYourLoan from "./components/sections/get-your-loan";
+import Why from "./components/sections/why";
 
 export default function Home() {
   return (
     <div >
       <Banner />
       <GetYourLoan />
+      <Why />
     </div>
   );
 }
